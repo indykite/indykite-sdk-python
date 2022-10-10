be6912b8da9c17ddbcc1826e76afb29345b1f1cb
gg <gg.mouette@indykite.com>
2022-10-10 16:35:22 +0200

    feat: add config methods 2798

--- tests/test_get_digital_twin.py
+++ tests/test_get_digital_twin.py
@@ -36,6 +36,20 @@ def test_get_digital_twin_wrong_tenant_id(capsys):
     assert response is None
 
 
+def test_get_digital_twin_wrong_format_tenant_id(capsys):
+    digital_twin_id = "534729fb-f1b9-43ad-b1c5-9bbc75ae7de8"
+    tenant_id = "gid:AAAAA2luZHlraURlgAADDwAAAAE"
+
+    client = IdentityClient()
+    assert client is not None
+
+    response = client.get_digital_twin(digital_twin_id, tenant_id, [])
+    captured = capsys.readouterr()
+
+    assert captured.out == "The tenant id is not in UUID4 format:\nbadly formed hexadecimal UUID string\n"
+    assert response is None
+
+
 def test_get_digital_twin_nonexisting_twin_id(capsys):
     digital_twin_id = "696e6479-6b69-4465-8000-030f00000001"
     tenant_id = data.get_tenant()
