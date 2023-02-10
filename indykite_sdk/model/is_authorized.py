8091e5d1627f6d5fc1a04d3418f278ccde9bedd7
gg <cowan.macady@gmail.com>
2023-02-10 11:57:42 +0100

    fix: corrected is_authorized deserialization and tests

--- indykite_sdk/model/is_authorized.py
+++ indykite_sdk/model/is_authorized.py
@@ -1,5 +1,6 @@
 from indykite_sdk.utils import timestamp_to_date
 from google.protobuf.json_format import MessageToJson, MessageToDict
+import indykite_sdk.utils.logger as logger
 
 
 class IsAuthorizedResponse:
@@ -8,15 +9,16 @@ class IsAuthorizedResponse:
         if message is None:
             return None
 
-        message_dict = MessageToDict(message, preserving_proto_field_name=True)
-        if message_dict and message_dict["decisions"]:
-            is_authorized_response = IsAuthorizedResponse(
-                decision_time=timestamp_to_date(message.decision_time),
-                decisions=message_dict["decisions"]
-            )
-        else:
-            is_authorized_response = IsAuthorizedResponse(decision_time=None, decisions={})
-        return is_authorized_response
+        try:
+            message_dict = MessageToDict(message, preserving_proto_field_name=True)
+            if message_dict and message_dict["decisions"]:
+                is_authorized_response = IsAuthorizedResponse(
+                    decision_time=timestamp_to_date(message.decision_time),
+                    decisions=message_dict["decisions"]
+                )
+            return is_authorized_response
+        except Exception as exception:
+            return logger.logger_error(exception)
 
     def __init__(self, decision_time, decisions):
         self.decision_time = decision_time,
